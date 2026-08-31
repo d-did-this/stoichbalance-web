fc5e155d6f4af3f01609c180c076027dcf94127e
d-did-this <samygg0316@gmail.com>
2026-09-01 04:01:44 +0800

    Apply mature UI JS dropdown logic and SVGs

--- script.js
+++ script.js
@@ -1109,3 +1109,17 @@ function returnToMenu() {
     }
     init();
 }
+
+
+// Dropdown Logic
+window.onclick = function(event) {
+    if (!event.target.closest('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
