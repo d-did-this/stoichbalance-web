ab516e017187132da6976d18b9315ca7da78d7dc
d-did-this <samygg0316@gmail.com>
2026-09-01 04:50:14 +0800

    Replace broken SVG paths with clean Heroicons

--- script.js
+++ script.js
@@ -1132,9 +1132,9 @@ function updateFullscreenIcon() {
     const isFull = document.fullscreenElement || document.webkitFullscreenElement || document.msFullscreenElement;
     if (isFull) {
         // Compress Icon
-        fsIcon.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M9 9L4 4m5 5V4m0 5H4m7 11l-5 5m5-5v5m0-5H4m16-6l-5-5m5 5v-5m0 5h-5m5 11l-5-5m5 5v-5m0 5h-5"></path>';
+        fsIcon.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-3.75 3.75M15 9h4.5M15 9V4.5M15 9l3.75-3.75M15 15h4.5M15 15v4.5m0-4.5l3.75 3.75"></path>';
     } else {
         // Expand Icon
-        fsIcon.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4"></path>';
+        fsIcon.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"></path>';
     }
 }
