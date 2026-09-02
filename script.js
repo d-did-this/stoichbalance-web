6311967efd1c274370793dac9ca30b6b7a250bbf
d-did-this <samygg0316@gmail.com>
2026-09-02 12:08:19 +0800

    Fix duplicate const syntax error crashing the Javascript engine

--- script.js
+++ script.js
@@ -843,9 +843,6 @@ function loadReaction(idx) {
             
             mCard.className = "master-card card";
             
-            const mText = document.getElementById('ms-status-text');
-            const mEmoji = document.getElementById('ms-emoji');
-            
             // Triangle > < =
             const pinText = document.getElementById('pin-text');
             if (!pinText) {
@@ -1182,4 +1179,5 @@ function updateFullscreenIcon() {
         fsIcon.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"></path>';
     }
 }
+
 
